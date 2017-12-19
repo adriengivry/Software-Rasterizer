07e4707e0f552d65cc49f8d409a9ca8b2875bd41
Adrien GIVRY <adrien@givry.pro>
2017-12-19 16:36:44 -0500

    Updating main to write information on console

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,29 @@
 #include <SDL.h>
 #include <cstdlib>
+#include <iostream>
 
-int main(int argc, char** argv)
+#define SDL_INIT_SUCCESS 0
+#define SDL_LOG(logMessage) std::cout << "[SDL] " << logMessage << std::endl
+
+int SDL_main(int argc, char** argv)
 {
-	if (SDL_Init(SDL_INIT_VIDEO))
 	{
+		SDL_LOG("Attempting to init");
+		if (SDL_Init(SDL_INIT_VIDEO) != SDL_INIT_SUCCESS)
+		{
+			SDL_LOG("Initialization failed");
+			return EXIT_FAILURE;
+		}
+
+		SDL_LOG("Initialization succeed");
+
 		// TODO Core of the program
+
 		SDL_Quit();
+		SDL_LOG("Closed");
 	}
-	else
-	{
-		return EXIT_FAILURE;
-	}
+
+	system("PAUSE");
+
 	return EXIT_SUCCESS;
 }
